a56227ab54f130e45de3957e084077cda971c159
GITHUB TOLOL™ <73346737+R3yy@users.noreply.github.com>
2021-09-08 19:27:49 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -13,6 +13,7 @@ sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Sukses :)
+echo MINNING, HACKING, PHISSING ? BANNED YOUR ACCOUNT !
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
