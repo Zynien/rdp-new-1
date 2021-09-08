23cc96b4dd2b08fd44a27a3dea6a8fdeb1c5659c
GITHUB TOLOL™ <73346737+R3yy@users.noreply.github.com>
2021-09-08 23:41:50 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -4,6 +4,5 @@ tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK t
 :check
 ping 127.0.0.1 > null
 cls
-echo • SERVER IN MAINTENANCE •
+echo xXX Server Maintenance XXx
 goto check
-
