d93d9e25ef5ebb71433d739ea450bbdc99a9ec2b
GITHUB TOLOL™ <73346737+R3yy@users.noreply.github.com>
2021-09-09 14:53:16 +0700

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,8 +1,6 @@
 @echo off
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.png /f /timer:0
-@D:\a\R3yy-RDP\R3yy-RDP\RULES.txt
-@RD /S /Q wallpaper
+RED ADD "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.png /f /timer:0
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im explorer.exe
 start explorer.exe
