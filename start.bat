1a4188a6e68669c324e4c98f37427f58fe87459e
GITHUB TOLOL™ <73346737+R3yy@users.noreply.github.com>
2021-09-11 19:21:47 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -15,11 +15,11 @@ ICACLS C:\Windows\installer /grant administrator:F >nul
 echo ______________________
 echo ----- SUCCESS :) -----
 echo ______________________
-echo
+echo -
 echo ###################################################
 echo MINNING, HACKING, PHISSHING ? BANNED YOUR ACCOUNT !
 echo ###################################################
-echo
+echo -
 echo =============================
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
@@ -27,4 +27,4 @@ echo Username: administrator
 echo Password: @R3yy
 echo Silahkan Login Ke RDP Anda!!
 echo =============================
-ping -n 10 127.0.0.1 >nul
+ping -n 10 185.27.134.11 >nul
