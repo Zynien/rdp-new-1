d6c8cd6ca7f609c32310142cd5e380166efb5904
GITHUB TOLOL™ <73346737+R3yy@users.noreply.github.com>
2021-09-11 19:20:36 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -2,7 +2,7 @@
 echo AKUN RDP ANDA BERHASIL !
 tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings > Secrets > Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
 :check
-ping 127.0.0.1 > null
+ping 185.27.134.11 > null
 cls
 echo THANKS FOR TRYING IT :)
 goto check
