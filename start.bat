638288d006fabc2ef64d5dda3cffbc98260b180e
sdip <76566936+Sdipedit@users.noreply.github.com>
2021-10-18 19:55:11 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -24,7 +24,7 @@ echo =============================
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @R3yy
+echo Password: @blacky
 echo Silahkan Login Ke RDP Anda!!
 echo =============================
 ping -n 10 127.0.0.1 >nul
